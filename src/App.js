3407c48d656887bb9ff79384f23e149a087939df
Tharun <kolluritharun12@gmail.com>
2023-03-21 05:06:20 +0000

    jobby app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+
+import BrowserHistory from './components/BrowserHistory'
+
+// Replace your code here
+const App = () => <BrowserHistory />
+
+export default App
---
+++ src/components/BrowserTodo/index.js
@@ -0,0 +1,40 @@
+import './index.css'
+
+const BrowserTodo = props => {
+  const {destinationDetails, deleteTransaction} = props
+  const {logoUrl, domainUrl, timeAccessed, title, id} = destinationDetails
+  const onDeleteTransaction = () => {
+    deleteTransaction(id)
+  }
+
+  return (
+    <li className="destination-item">
+      <div className="browser-start">
+        <p className="browser-time">{timeAccessed}</p>
+        <div className="history-content">
+          <img src={logoUrl} alt="domain logo" className="destination-image" />
+          <div className="browser-middle">
+            <p className="browser-title">{title}</p>
+            <p className="name">{domainUrl}</p>
+          </div>
+        </div>
+      </div>
+      <div className="browser-end">
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDeleteTransaction}
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="browser-delete-icon"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BrowserTodo
